5a2705375a0637e96b15e9fc3a13b4dedf09d2f5
leo <celsolnv27@gmail.com>
2022-02-10 14:05:13 -0300

    Preparando revisão de código

--- src/database.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const mongoClient = await new MongoClient(process.env.MONGO_URI).connect();
-
-const db = mongoClient.db("my-wallet-db");
-
-export default db;
--- src/schemas/entrySchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const entrySchema = joi.object({
-  price: joi.number().required(),
-  description: joi.string().required(),
-  type: joi.string().valid('income', 'expense')
-});
-
-export default entrySchema;
